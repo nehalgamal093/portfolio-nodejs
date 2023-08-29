380bc80057e1ce37cf8363631492169196e4ed42
nehalgamal093 <nehalgamal093@gmail.com>
2023-08-29 21:16:57 +0300

    fix

--- src/modules/middleware/fileUpload.js
+++ src/modules/middleware/fileUpload.js
@@ -4,7 +4,8 @@ import multer from 'multer';
 let options = (folderName) =>{
     const storage = multer.diskStorage({
         destination:function(req,file,cb){
-            cb(null,`uploads/${folderName}`);
+            cb(null,`${process.cwd()}/uploads/${folderName}`);
+            console.log("🚀 ~ file: upload.ts:11 ~ file", process.cwd());
         },
         filename:function(req,file,cb){
             const uniqueSuffix = Date.now() + "-" +Math.round(Math.random() * 1e9);
