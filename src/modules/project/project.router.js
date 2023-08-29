d4e2642c3c1fcc6201550717cf3621da8fcaaa1a
nehalgamal093 <nehalgamal093@gmail.com>
2023-08-29 13:50:47 +0300

    fix

--- src/modules/project/project.router.js
+++ src/modules/project/project.router.js
@@ -1,14 +1,14 @@
 import express from "express";
 import * as project from "./project.controller.js";
-// import { uploadMixOfFiles } from "../middleware/fileUpload.js";
+import { uploadMixOfFiles } from "../middleware/fileUpload.js";
 
 const projectRouter = express.Router();
 
-// let fieldsArray = [{ name: "images", maxCount: 10 }];
+let fieldsArray = [{ name: "images", maxCount: 10 }];
 
 projectRouter
   .route("/")
-  .post(project.createProject)
+  .post(uploadMixOfFiles(fieldsArray, "project"), project.createProject)
   .get(project.getAllProjects);
 
 
