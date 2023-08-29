4e210bd4cbc17af785775addd866a0e597fde1ec
nehalgamal093 <nehalgamal093@gmail.com>
2023-08-29 14:37:33 +0300

    fix

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import certificateRouter from "./src/modules/certificate/certificate.router.js";
 dotenv.config()
 const app = express();
 
-const port = 3001;
+// const port = 3001;
 app.use(cors())
 app.use(express.json());
 
@@ -24,7 +24,7 @@ app.get("/", (req, res) => {
 
 dbConnection();
 
-app.listen(port, "0.0.0.0", () => {
+app.listen("0.0.0.0", () => {
   console.log("Connected successfully");
 });
 
