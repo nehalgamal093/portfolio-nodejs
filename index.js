c3b9500ba57a5586deb6c77441bb2e5101b689e8
nehalgamal093 <nehalgamal093@gmail.com>
2023-08-29 19:42:31 +0300

    fix

--- index.js
+++ index.js
@@ -5,21 +5,14 @@ import * as dotenv from "dotenv";
 import cors from 'cors';
 import profileRouter from "./src/modules/profile/profile.router.js";
 import certificateRouter from "./src/modules/certificate/certificate.router.js";
-import { fileURLToPath } from 'url';
-import path from "path";
 dotenv.config()
 const app = express();
 
-const port = 3001;
+// const port = 3001;
 app.use(cors())
 app.use(express.json());
 
-
-const __filename = fileURLToPath(import.meta.url);
-
-const __dirname = path.dirname(__filename);
-//  app.use(express.static("uploads"));
-app.use(express.static(__dirname));
+ app.use(express.static("uploads"));
 app.use("/projects", projectRouter);
 app.use("/profiles", profileRouter);
 app.use("/certificates", certificateRouter);
@@ -31,7 +24,7 @@ app.get("/", (req, res) => {
 
 dbConnection();
 
-app.listen(port,"0.0.0.0", () => {
+app.listen("0.0.0.0", () => {
   console.log("Connected successfully");
 });
 
--- src/modules/middleware/fileUpload.js
+++ src/modules/middleware/fileUpload.js
@@ -1,18 +1,13 @@
 import multer from 'multer';
-import path from 'path';
 
-import { fileURLToPath } from 'url';
 
-const __filename = fileURLToPath(import.meta.url);
-
-const __dirname = path.dirname(__filename);
 let options = (folderName) =>{
     const storage = multer.diskStorage({
         destination:function(req,file,cb){
-            cb(null,path.join(__dirname,`uploads/${folderName}`));
+            cb(null,`uploads/${folderName}`);
         },
         filename:function(req,file,cb){
-            const uniqueSuffix = Date().now().replace(/:/g, '-') + "-" +Math.round(Math.random() * 1e9);
+            const uniqueSuffix = Date.now() + "-" +Math.round(Math.random() * 1e9);
             cb(null,uniqueSuffix+'-'+file.originalname);
         }
     })
