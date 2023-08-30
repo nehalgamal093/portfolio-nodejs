95c8e20f34ef34fff1ec1f4879d4a02f633b3db4
nehalgamal093 <nehalgamal093@gmail.com>
2023-08-31 02:06:24 +0300

    fix

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const port = 3001;
 app.use(cors())
 app.use(express.json());
 
- app.use(express.static(path.join(process.cwd(),'static')));
+ app.use(express.static('static'));
 app.use("/projects", projectRouter);
 app.use("/profiles", profileRouter);
 app.use("/certificates", certificateRouter);
