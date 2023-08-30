2c3403f16552ae61a6d01bda8bd608bdd152aa88
nehalgamal093 <nehalgamal093@gmail.com>
2023-08-30 14:56:40 +0300

    fixthree

---
+++ src/modules/middleware/fileUpload.js
@@ -0,0 +1,29 @@
+import multer from 'multer';
+import path from 'path';
+
+let options = (folderName) =>{
+    const storage = multer.diskStorage({
+        destination:function(req,file,cb){
+            cb(null,process.cwd()+`${folderName}`);
+
+            console.log(`🚀 ${process.cwd()} +${folderName}`);
+        },
+        filename:function(req,file,cb){
+            const uniqueSuffix = Date.now() + "-" +Math.round(Math.random() * 1e9);
+            cb(null,uniqueSuffix+'-'+file.originalname);
+        }
+    })
+
+    function fileFilter(req,file,cb){
+        if(file.mimetype.startsWith('image')){
+            cb(null,true)
+        } else {
+            cb("Error pic",false)
+        }
+    }
+    return multer({storage,fileFilter});
+}
+export const uploadSingleFile = (fieldName,folderName) => 
+options(folderName).single(fieldName)
+export const uploadMixOfFiles = (arrayOfFields,folderName) =>
+options(folderName).fields(arrayOfFields)
