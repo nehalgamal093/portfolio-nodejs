651a5784bafdc42f4799d14a11553d242beac2e1
nehalgamal093 <nehalgamal093@gmail.com>
2024-02-17 10:44:53 +0200

    cors

--- index.js
+++ index.js
@@ -2,23 +2,26 @@ import express from "express";
 import { dbConnection } from "./databaseConnection/dbConnection.js";
 import projectRouter from "./src/modules/project/project.router.js";
 import * as dotenv from "dotenv";
-import cors from 'cors';
+import cors from "cors";
 import profileRouter from "./src/modules/profile/profile.router.js";
 import certificateRouter from "./src/modules/certificate/certificate.router.js";
 
-dotenv.config()
+dotenv.config();
 const app = express();
-
+const corsOptions = {
+  origin: "*",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
 const port = 3001;
-app.use(cors())
+app.use(cors(corsOptions));
 app.use(express.json());
-app.use(express.urlencoded({extended:false}))
+app.use(express.urlencoded({ extended: false }));
 
 //  app.use(express.static('public'));
 app.use("/projects", projectRouter);
 app.use("/profiles", profileRouter);
 app.use("/certificates", certificateRouter);
-
 
 app.get("/", (req, res) => {
   res.send("Hello world");
@@ -26,8 +29,6 @@ app.get("/", (req, res) => {
 
 dbConnection();
 
-app.listen(port,"0.0.0.0", () => {
+app.listen(port, "0.0.0.0", () => {
   console.log("Connected successfully");
 });
-
-
